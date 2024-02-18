4545a636d2a5d809c3f1c80239ad8386e920bc87
Marco Castanho <marco.castanho@toptal.com>
2024-02-18 14:32:24 +0000

    graphql-request

---
+++ client/src/lib/graphql/queries.js
@@ -0,0 +1,21 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const client = new GraphQLClient("http://localhost:9000/graphql");
+
+export const getJobs = async () => {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await client.request(query);
+  return jobs;
+};
