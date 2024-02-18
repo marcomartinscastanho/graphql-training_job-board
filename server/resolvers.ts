f921e23ee3cbafd90b1837d0891f450cc5b57805
Marco Castanho <marco.castanho@toptal.com>
2024-02-18 14:46:44 +0000

    query arguments

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,12 +1,20 @@
 import { getCompany } from "./db/companies";
-import { getJobs } from "./db/jobs";
+import { getJob, getJobs } from "./db/jobs";
 
 export const resolvers = {
   Query: {
+    // the first parameter of this resolver is the root object, which is useless here
+    // the second parameter however is the request arguments object
+    job: (_root, args) => {
+      const { id } = args;
+      return getJob(id);
+    },
     jobs: () => getJobs(),
   },
 
   Job: {
+    // resolver function always receives the parent object as the first parameter
+    // which in this case is the job object
     date: (job) => job.createdAt.slice(0, "yyyy-mm-dd".length),
     company: (job) => getCompany(job.companyId),
   },
